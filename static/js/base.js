aa8385cd8b76e4516747909f5c2bc96a7bf06a58
Zach Lagden <lagdenzach7756@gmail.com>
2023-03-20 22:08:12 +0000

    first commit

---
+++ static/js/base.js
@@ -0,0 +1,15 @@
+const $buoop = {
+    required: { e: -3, f: -3, o: -3, s: -1, c: -3 },
+    insecure: true,
+    unsupported: true,
+    api: 2023.03,
+};
+
+function $buo_f() {
+    const e = document.createElement("script");
+    e.src = "//browser-update.org/update.min.js";
+    document.body.appendChild(e);
+}
+
+document.addEventListener("DOMContentLoaded", $buo_f, false);
+window.addEventListener('load', $buo_f);
